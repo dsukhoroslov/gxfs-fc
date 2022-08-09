57f45643c6c653a94f3199047e93adaab7e9f45b
Philipp Hertweck <philipp.hertweck@iosb.fraunhofer.de>
2022-08-09 12:14:48 +0200

    Enable docker in .gitlab-ci to run integration tests.

--- fc-service-core/src/test/java/eu/gaiax/difs/fc/core/service/graphdb/GraphTest.java
+++ fc-service-core/src/test/java/eu/gaiax/difs/fc/core/service/graphdb/GraphTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@Disabled("Not running due to missing docker in CI environment")
 public class GraphTest {
 
     GraphConnect graphGaia;
