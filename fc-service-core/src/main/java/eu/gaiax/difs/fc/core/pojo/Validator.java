86ed31876c20a1b598fe9425bbe4f1c313ac336f
Sebastian Duda <sebastian.duda@fit.fraunhofer.de>
2022-08-29 11:32:25 +0000

    Add getters and setters to Validator

--- fc-service-core/src/main/java/eu/gaiax/difs/fc/core/pojo/Validator.java
+++ fc-service-core/src/main/java/eu/gaiax/difs/fc/core/pojo/Validator.java
@@ -2,16 +2,37 @@ package eu.gaiax.difs.fc.core.pojo;
 
 import java.time.Instant;
 
-
 /**
  * POJO Class for holding the validators, that signed the Self-Description.
  */
 @lombok.Getter
-@lombok.Setter
 public class Validator {
-    String didURI;
+    private String didURI;
 
-    String publicKey;
+    private String publicKey;
 
-    Instant expirationDaten;
+    private Instant expirationDaten;
+
+    private boolean changed = false;
+
+    public Validator (String didURI, String publicKey, Instant expirationDaten) {
+        this.didURI = didURI;
+        this.publicKey = publicKey;
+        this.expirationDaten = expirationDaten;
+    }
+
+    public void setDidURi (String didURI) {
+        this.didURI = didURI;
+        changed = true;
+    }
+
+    public void setPublicKey (String publicKey) {
+        this.publicKey = publicKey;
+        changed = true;
+    }
+
+    public void setExpirationDate (Instant expirationDaten) {
+        this.expirationDaten = expirationDaten;
+        changed = true;
+    }
 }
