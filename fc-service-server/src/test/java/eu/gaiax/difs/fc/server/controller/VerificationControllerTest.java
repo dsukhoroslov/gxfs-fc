24025c8044cd5c52aa92bfaeec9a55271db7aedf
Hylke van der Schaaf <hylke.vds@gmail.com>
2022-11-16 20:25:10 +0100

    Cleaned up initialisation of schema in VerificationControllerTest

--- fc-service-server/src/test/java/eu/gaiax/difs/fc/server/controller/VerificationControllerTest.java
+++ fc-service-server/src/test/java/eu/gaiax/difs/fc/server/controller/VerificationControllerTest.java
@@ -1,19 +1,17 @@
 package eu.gaiax.difs.fc.server.controller;
 
-import eu.gaiax.difs.fc.core.service.filestore.FileStore;
-import eu.gaiax.difs.fc.core.service.schemastore.SchemaStore;
 import eu.gaiax.difs.fc.core.service.schemastore.impl.SchemaStoreImpl;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -37,123 +35,112 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureEmbeddedDatabase(provider = DatabaseProvider.ZONKY)
 public class VerificationControllerTest {
 
-    @Autowired
-    private WebApplicationContext context;
-    @Autowired
-    private ObjectMapper objectMapper;
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private SchemaStoreImpl schemaStore;
+  @Autowired
+  private WebApplicationContext context;
 
-    @Autowired
-    @Qualifier("schemaFileStore")
-    private FileStore fileStore;
+  @Autowired
+  private ObjectMapper objectMapper;
 
+  @Autowired
+  private MockMvc mockMvc;
 
+  @Autowired
+  private SchemaStoreImpl schemaStore;
 
-
-    @BeforeTestClass
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
-    }
-    @AfterEach
-    public void storageSelfCleaning() throws IOException {
-        Map<SchemaStore.SchemaType, List<String>> schemaList = schemaStore.getSchemaList();
-        for (List<String> typeList : schemaList.values()) {
-            for (String schema : typeList) {
-                schemaStore.deleteSchema(schema);
-            }
-        }
-        fileStore.clearStorage();
-    }
-    @Test
-    public void getVerifyPageShouldReturnSuccessResponse() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/verification")
-               .contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON))
-               .andExpect(status().isOk())
-               .andExpect(header().stringValues("Content-Type", "text/html"));
-    }
-
-    @Test
-    public void verifyParticipantShouldReturnSuccessResponse() throws Exception {
-        String json = getMockFileDataAsString("default_participant.json");
+  @BeforeAll
+  public void setup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
     schemaStore.addSchema(getAccessor("mock-data/gax-test-ontology.ttl"));
-        String response = mockMvc.perform(MockMvcRequestBuilders.post("/verification")
-               .contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON)
-               .content(json))
-               .andExpect(status().isOk())
-               .andReturn()
-               .getResponse()
-               .getContentAsString();
-        VerificationResult partResult = objectMapper.readValue(response, VerificationResult.class);
-        assertEquals("did:example:issuer", partResult.getIssuer());
-        assertEquals(Instant.parse("2010-01-01T19:23:24Z"), partResult.getIssuedDateTime()); 
-    }
- 
-    @Test
-    public void verifyNoProofsShouldReturnUnprocessibleEntity() throws Exception {
-        String json = getMockFileDataAsString("participant_without_proofs.json");
-        schemaStore.addSchema(getAccessor("mock-data/gax-test-ontology.ttl"));
-        mockMvc.perform(MockMvcRequestBuilders.post("/verification")
-               .contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON)
-               .content(json))
-               .andExpect(status().isUnprocessableEntity());
-    }
-    
-    @Test
-    public void verifyNoProofsNoSignsShouldReturnSuccessResponse() throws Exception {
-        String json = getMockFileDataAsString("participant_without_proofs.json");
-        schemaStore.addSchema(getAccessor("mock-data/gax-test-ontology.ttl"));
-        mockMvc.perform(MockMvcRequestBuilders.post("/verification")
-               .queryParam("verifySemantics", "false")
-               .queryParam("verifySignatures", "false")
-               .contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON)
-               .content(json))
-               .andExpect(status().isOk());
-    }
+  }
 
-    @Test
-    public void verifySDNoCSShouldReturnUnprocessibleEntity() throws Exception {
-        String json = getMockFileDataAsString("sd-without-credential-subject.json");
-        String response = mockMvc.perform(MockMvcRequestBuilders.post("/verification")
-               .contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON)
-               .content(json))
-               .andExpect(status().isUnprocessableEntity())
-               .andReturn()
-               .getResponse()
-               .getContentAsString();
-        Error error = objectMapper.readValue(response, Error.class);
-        assertEquals("verification_error", error.getCode());
-        assertTrue(error.getMessage().startsWith("Semantic Errors:"), "Message is: " + error.getMessage());
-        assertTrue(error.getMessage().contains("must contain 'verifiableCredential' property"), "Message is: " + error.getMessage());
-    }
+  @AfterAll
+  public void storageSelfCleaning() throws IOException {
+    schemaStore.clear();
+  }
 
-    @Test
-    @Disabled("I don't see where this test is supposed to succeed")
-    public void verifySDNoCSNoSemanticsShouldReturnSuccessResponse() throws Exception {
-        String json = getMockFileDataAsString("sd-without-credential-subject.json");
-        mockMvc.perform(MockMvcRequestBuilders.post("/verification")
-               .queryParam("verifySemantics", "false")
-               .contentType(MediaType.APPLICATION_JSON)
-               .accept(MediaType.APPLICATION_JSON)
-               .content(json))
-               .andExpect(status().isOk());
-    }
-    
+  @Test
+  public void getVerifyPageShouldReturnSuccessResponse() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/verification")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(header().stringValues("Content-Type", "text/html"));
+  }
+
+  @Test
+  public void verifyParticipantShouldReturnSuccessResponse() throws Exception {
+    String json = getMockFileDataAsString("default_participant.json");
+    String response = mockMvc.perform(MockMvcRequestBuilders.post("/verification")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    VerificationResult partResult = objectMapper.readValue(response, VerificationResult.class);
+    assertEquals("did:example:issuer", partResult.getIssuer());
+    assertEquals(Instant.parse("2010-01-01T19:23:24Z"), partResult.getIssuedDateTime());
+  }
+
+  @Test
+  public void verifyNoProofsShouldReturnUnprocessibleEntity() throws Exception {
+    String json = getMockFileDataAsString("participant_without_proofs.json");
+    mockMvc.perform(MockMvcRequestBuilders.post("/verification")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isUnprocessableEntity());
+  }
+
+  @Test
+  public void verifyNoProofsNoSignsShouldReturnSuccessResponse() throws Exception {
+    String json = getMockFileDataAsString("participant_without_proofs.json");
+    mockMvc.perform(MockMvcRequestBuilders.post("/verification")
+            .queryParam("verifySemantics", "false")
+            .queryParam("verifySignatures", "false")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  public void verifySDNoCSShouldReturnUnprocessibleEntity() throws Exception {
+    String json = getMockFileDataAsString("sd-without-credential-subject.json");
+    String response = mockMvc.perform(MockMvcRequestBuilders.post("/verification")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isUnprocessableEntity())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    Error error = objectMapper.readValue(response, Error.class);
+    assertEquals("verification_error", error.getCode());
+    assertTrue(error.getMessage().startsWith("Semantic Errors:"), "Message is: " + error.getMessage());
+    assertTrue(error.getMessage().contains("must contain 'verifiableCredential' property"), "Message is: " + error.getMessage());
+  }
+
+  @Test
+  @Disabled("I don't see where this test is supposed to succeed")
+  public void verifySDNoCSNoSemanticsShouldReturnSuccessResponse() throws Exception {
+    String json = getMockFileDataAsString("sd-without-credential-subject.json");
+    mockMvc.perform(MockMvcRequestBuilders.post("/verification")
+            .queryParam("verifySemantics", "false")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(json))
+            .andExpect(status().isOk());
+  }
+
 }
