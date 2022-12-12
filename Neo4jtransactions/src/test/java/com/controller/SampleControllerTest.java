9fec620046134c5b7c22e942ab799e2f692f8349
Denis Sukhoroslov <denis.sukhoroslov@t-systems.com>
2022-12-12 17:53:18 +0200

    adding test

---
+++ Neo4jtransactions/src/test/java/com/controller/SampleControllerTest.java
@@ -0,0 +1,41 @@
+package com.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+//import eu.gaiax.difs.fc.testsupport.config.EmbeddedNeo4JConfig;
+//import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+//import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
+//@AutoConfigureEmbeddedDatabase(provider = DatabaseProvider.ZONKY)
+//@Import(EmbeddedNeo4JConfig.class)
+public class SampleControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void testSample() throws Exception {
+        
+        mockMvc.perform(MockMvcRequestBuilders.post("/test"))
+            .andExpect(status().isOk());
+    }
+
+}
